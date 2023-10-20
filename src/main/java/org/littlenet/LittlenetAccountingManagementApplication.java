801c85898fdb1fbeafb5878a1cd9d2c836f78780
Anton Kr <antnjavadev@gmail.com>
2023-10-20 19:48:51 +0300

    create security

---
+++ src/main/java/org/littlenet/LittlenetAccountingManagementApplication.java
@@ -0,0 +1,13 @@
+package org.littlenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlenetAccountingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittlenetAccountingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/littlenet/LittlenetAccountingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.littlenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittlenetAccountingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
